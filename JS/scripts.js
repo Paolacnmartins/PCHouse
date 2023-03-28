fcda0657b4cfed25d9ff03a4bc1615bac9df7e58
Paola Martins <paola.nmartins@gmail.com>
2023-03-27 21:54:02 -0300

    paolapchouse

---
+++ JS/scripts.js
@@ -0,0 +1,23 @@
+console.log("Primeiro");
+
+console.log("último");
+
+console.log("mais um console.log");
+
+// declara uma variável
+var a= 1;
+a = 'teste';
+console.log(a);
+
+
+//este é um comentário
+
+/*
+ este
+ é
+ um 
+ comentário
+ de
+ multiplas
+ linhas
+ */
---
+++ JS/scriptvar.js
@@ -0,0 +1,13 @@
+var teste = 1;
+
+console.log(teste);
+
+teste = 'PAOLA';
+
+console.log(teste);
+
+let testando = 100;
+const ola = 200;
+
+console.log(testando);
+console.log(ola);
